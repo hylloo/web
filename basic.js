4f9e9f93f32e70b9d5f419982f79c3130acc5771
Iiro Pitkänen <106667418+hylloo@users.noreply.github.com>
2025-06-06 12:18:03 +0300

    tehty kokonaan

--- basic.js
+++ basic.js
@@ -1,1 +1,21 @@
-console.log("Hello, World!");
+console.log("hello world");
+
+
+const buttonName = document.getElementById("my-button");
+const listButton = document.getElementById("my-list");
+
+buttonName.addEventListener("click", function() {
+    const nameInput = document.getElementById("h1");
+    h1.textContent = "Moi maailma";
+    
+})
+
+
+
+listButton.addEventListener("click", function() {
+    const list = document.getElementById("list");
+    const newItem = document.createElement("li");
+    newItem.textContent = "New Item";
+    list.appendChild(newItem);
+    
+})
