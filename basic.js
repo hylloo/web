54988dd0206db09d09a428c9a62194f768f59a58
Iiro Pitkänen <106667418+hylloo@users.noreply.github.com>
2025-06-06 12:59:00 +0300

    q.

--- basic.js
+++ basic.js
@@ -2,7 +2,9 @@ console.log("hello world");
 
 
 const buttonName = document.getElementById("my-button");
-const listButton = document.getElementById("my-list");
+const listButton = document.getElementById("add-data");
+var area = document.getElementById("area");
+var list = document.getElementById("list");
 
 buttonName.addEventListener("click", function() {
     const nameInput = document.getElementById("h1");
@@ -13,9 +15,14 @@ buttonName.addEventListener("click", function() {
 
 
 listButton.addEventListener("click", function() {
-    const list = document.getElementById("list");
-    const newItem = document.createElement("li");
-    newItem.textContent = "New Item";
-    list.appendChild(newItem);
+    
+    const listItem = document.createElement("li");
+    let textToAdd = area.value;  
+    listItem.append(textToAdd);
+    console.log(textToAdd);
+    list.append(listItem);
+    area.value = "";
+
+
     
 })
